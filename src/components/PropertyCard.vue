ff847b734fa4a31da196897b60b0e23315c79dab
Nigel <nigel.stirzaker@nielsen.com>
2020-08-24 18:05:28 +0100

    Starting point for "Structuring a Basic Card"

---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white">
+    <img :src="property.imageUrl" :alt="property.imageAlt">
+    <div class="p-6">
+      <h4>{{ property.title}}</h4>
+      <div>
+        {{property.beds}} beds &bull; {{property.baths}} baths
+      </div>
+      <div>
+        {{property.formattedPrice}} /wk
+      </div>
+      <div>
+        {{property.rating}}/5 stars based on {{property.reviewCount}} reviews
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['property'],
+  name: "PropertyCard"
+}
+</script>
+
+<style scoped>
+
+</style>
